7eb888d671e1e1caeadf946ff04d5db95c0dbcf8
Truong Cong Thanh <thanhtruong@toancausoft.com.vn>
2014-10-15 17:04:27 +0700

    - Layout danh mục + Tỉnh/TP , phân trang bằng ajax, tìm kiếm

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,20 +4,6 @@
 //= require bootstrap.min
 //= require_tree .
 
-function show_confirm_modal(path, question){
-    $('#confirm-modal #question').text(question);
-    $('#confirm-modal #delete').on("click", function(){
-        $.ajax({
-            async: false,
-            type: "DELETE",
-            url: path,
-            success: function() {$('#confirm-modal').modal('hide');},
-            dataType: 'script'
-        });
-        $('#confirm-modal').modal('hide');
-    });
-    $('#confirm-modal').show();
-}
 $(function () {
     $("#tinhthanhphos").on("click", ".pagination a", function () {
         $.getScript(this.href);
