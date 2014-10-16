9fe7d57503b89fc5f4fc2835ba64363974ea802c
Truong Cong Thanh <thanhtruong@toancausoft.com.vn>
2014-10-16 11:01:20 +0700

    - Layout danh mục + Tỉnh/TP : đa ngôn ngữ

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  resources :tinhthanhphos
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
 
-  resources :vungdialies
-
-  resources :quoctiches
-
-  root to: 'pages#home', :as => 'home'
-
-  match '/about' => 'pages#about', :via => :get
-  match '/signin' => 'pages#signin', :via => [:get,:post]
-  match '/danhmuc' => 'categories#index', :via => :get
+    resources :tinhthanhphos
+    resources :vungdialies
+    resources :quoctiches
+    root to: 'pages#home', :as => 'home'
+    match '/about' => 'pages#about', :via => :get
+    match '/signin' => 'pages#signin', :via => [:get, :post]
+    match '/danhmuc' => 'categories#index', :via => :get
+  end
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}"), :via => [:get,:post]
+  match '', to: redirect("/#{I18n.default_locale}"), :via => [:get,:post]
 end
