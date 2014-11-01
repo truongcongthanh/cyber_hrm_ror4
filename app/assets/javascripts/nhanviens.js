b710ab04721b403c385473b3383c64052dfea31c
Truong Cong Thanh <thanhtruong@toancausoft.com.vn>
2014-11-01 08:58:05 +0700

    Quản lý nhân viên

--- app/assets/javascripts/nhanviens.js
+++ app/assets/javascripts/nhanviens.js
@@ -1,19 +1,16 @@
-$(function () {
-    $(".form-index tr").click(function () {
-        id = $(this).attr("nhanvienid");
-        $.ajax({
-            url: "laythongtinnhanvien",
-            dataType: 'json',
-            data: {id: id}
-        }).done(function (json) {
-                $("#nhanvien").find("h4").html(json.holot + " " + json.ten);
-                if (json.ngaysinh == '')
-                    $("#nhanvien").find("#ngaysinh").html(json.namsinh);
-                else
-                    $("#nhanvien").find("#ngaysinh").html(json.ngaysinh);
-            }
-        )
-        ;
-    });
-});
 
+function  aaa(_id) {
+    id = $(_id).attr("nhanvienid");
+    $.ajax({
+        url: "laythongtinnhanvien",
+        dataType: 'json',
+        data: {id: id}
+    }).done(function (json) {
+            $("#nhanvien").find("h4").html(json.holot + " " + json.ten);
+            if (json.ngaysinh == '')
+                $("#nhanvien").find("#ngaysinh").html(json.namsinh);
+            else
+                $("#nhanvien").find("#ngaysinh").html(json.ngaysinh);
+        }
+    );
+}
