386107e44eb0c39cd2a0069f49cbafdac50c37d3
Truong Cong Thanh <thanhtruong@toancausoft.com.vn>
2014-11-01 14:00:08 +0700

    Quản lý nhân viên

--- app/assets/javascripts/nhanviens.js
+++ app/assets/javascripts/nhanviens.js
@@ -15,10 +15,10 @@ function nhanvien(_id) {
     );
 }
 
-function nhanvien_gioitinh(id_gioitinh) {
+function nhanvien_gioitinh() {
     $("#nhanvien_gioitinh").click(function () {
         if ($(id_gioitinh).find("option:first-child").attr('key') == 0) {
-            $.get("gioitinh", function (json) {
+            $.get("/gioitinh", function (json) {
                 alert(JSON.stringify(json));
                 string = "<option><%= t 'nhanviens.prompt_gioitinh' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -33,7 +33,7 @@ function nhanvien_gioitinh(id_gioitinh) {
 function nhanvien_tinhtranghonnhan(id_honnhan) {
     $("#nhanvien_tinhtranghonnhan").click(function () {
         if ($(id_honnhan).find("option:first-child").attr('key') == 0) {
-            $.get("tinhtranghonnhan", function (json) {
+            $.get("/tinhtranghonnhan", function (json) {
                 string = "<option><%= t 'nhanviens.prompt_honnhan' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -47,7 +47,7 @@ function nhanvien_tinhtranghonnhan(id_honnhan) {
 function nhanvien_quoctich(id_quoctich) {
     $("#nhanvien_quoctich").click(function () {
         if ($(id_quoctich).find("option:first-child").attr('key') == 0) {
-            $.get("quoctich", function (json) {
+            $.get("/quoctich", function (json) {
                 string = "<option><%= t 'nhanviens.prompt_quoctich' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -61,7 +61,7 @@ function nhanvien_quoctich(id_quoctich) {
 function nhanvien_dantoc(id_dantoc) {
     $("#nhanvien_dantoc").click(function () {
         if ($(id_dantoc).find("option:first-child").attr('key') == 0) {
-            $.get("dantoc", function (json) {
+            $.get("/dantoc", function (json) {
                 string = "<option><%= t 'nhanviens.prompt_dantoc' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -75,7 +75,7 @@ function nhanvien_dantoc(id_dantoc) {
 function nhanvien_tongiao(id_tongiao) {
     $("#nhanvien_tongiao").click(function () {
         if ($(this).find("option:first-child").attr('key') == 0) {
-            $.get("tongiao", function (json) {
+            $.get("/tongiao", function (json) {
                 string = "<option><%= t 'nhanviens.prompt_tongiao' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -89,7 +89,7 @@ function nhanvien_tongiao(id_tongiao) {
 function nhanvien_noisinh_tinh(id_tinh) {
     $("#nhanvien_noisinh_tinh").click(function (event) {
         if ($(id_tinh).find("option:first-child").attr('key') == 0) {
-            $.get('danh_sach_tinh_tp', function (json) {
+            $.get('/danh_sach_tinh_tp', function (json) {
                 string = "<option><%= t 'nhanviens.prompt_tinhthanhphos' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -103,7 +103,7 @@ function nhanvien_noisinh_tinh(id_tinh) {
 function nhanvien_noisinh_huyen(id_huyen) {
     $("#nhanvien_noisinh_huyen").click(function (event) {
         if ($(id_huyen).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_quan_huyen', {id: $("#nhanvien_noisinh_tinh").val()}, function (json) {
+            $.post('/danh_sach_quan_huyen', {id: $("#nhanvien_noisinh_tinh").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_quanhuyens' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -118,7 +118,7 @@ function nhanvien_noisinh_huyen(id_huyen) {
 function nhanvien_noisinh_xa(id_xa) {
     $("#nhanvien_noisinh_xa").click(function (event) {
         if ($(id_xa).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_phuong_xa', {id: $("#nhanvien_noisinh_huyen").val()}, function (json) {
+            $.post('/danh_sach_phuong_xa', {id: $("#nhanvien_noisinh_huyen").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_phuongxas' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -133,7 +133,7 @@ function nhanvien_noisinh_xa(id_xa) {
 function nhanvien_nguyenquan_tinh(id_tinh) {
     $("#nhanvien_nguyenquan_tinh").click(function (event) {
         if ($(id_tinh).find("option:first-child").attr('key') == 0) {
-            $.get('danh_sach_tinh_tp', function (json) {
+            $.get('/danh_sach_tinh_tp', function (json) {
                 string = "<option><%= t 'nhanviens.prompt_tinhthanhphos' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -147,7 +147,7 @@ function nhanvien_nguyenquan_tinh(id_tinh) {
 function nhanvien_nguyenquan_huyen(id_huyen) {
     $("#nhanvien_nguyenquan_huyen").click(function (event) {
         if ($(id_huyen).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_quan_huyen', {id: $("#nhanvien_nguyenquan_tinh").val()}, function (json) {
+            $.post('/danh_sach_quan_huyen', {id: $("#nhanvien_nguyenquan_tinh").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_quanhuyens' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -162,7 +162,7 @@ function nhanvien_nguyenquan_huyen(id_huyen) {
 function nhanvien_nguyenquan_xa(id_xa) {
     $("#nhanvien_nguyenquan_xa").click(function (event) {
         if ($(id_xa).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_phuong_xa', {id: $("#nhanvien_nguyenquan_huyen").val()}, function (json) {
+            $.post('/danh_sach_phuong_xa', {id: $("#nhanvien_nguyenquan_huyen").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_phuongxas' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -177,7 +177,7 @@ function nhanvien_nguyenquan_xa(id_xa) {
 function nhanvien_thuongtru_tinh(id_tinh) {
     $("#nhanvien_thuongtru_tinh").click(function (event) {
         if ($(id_tinh).find("option:first-child").attr('key') == 0) {
-            $.get('danh_sach_tinh_tp', function (json) {
+            $.get('/danh_sach_tinh_tp', function (json) {
                 string = "<option><%= t 'nhanviens.prompt_tinhthanhphos' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -191,7 +191,7 @@ function nhanvien_thuongtru_tinh(id_tinh) {
 function nhanvien_thuongtru_huyen(id_huyen) {
     $("#nhanvien_thuongtru_huyen").click(function (event) {
         if ($(this).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_quan_huyen', {id: $("#nhanvien_thuongtru_tinh").val()}, function (json) {
+            $.post('/danh_sach_quan_huyen', {id: $("#nhanvien_thuongtru_tinh").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_quanhuyens' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -206,7 +206,7 @@ function nhanvien_thuongtru_huyen(id_huyen) {
 function nhanvien_thuongtru_xa(id_xa) {
     $("#nhanvien_thuongtru_xa").click(function (event) {
         if ($(id_xa).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_phuong_xa', {id: $("#nhanvien_thuongtru_huyen").val()}, function (json) {
+            $.post('/danh_sach_phuong_xa', {id: $("#nhanvien_thuongtru_huyen").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_phuongxas' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -221,7 +221,7 @@ function nhanvien_thuongtru_xa(id_xa) {
 function nhanvien_tamtru_tinh(id_tinh) {
     $("#nhanvien_tamtru_tinh").click(function (event) {
         if ($(id_tinh).find("option:first-child").attr('key') == 0) {
-            $.get('danh_sach_tinh_tp', function (json) {
+            $.get('/danh_sach_tinh_tp', function (json) {
                 string = "<option><%= t 'nhanviens.prompt_tinhthanhphos' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -235,7 +235,7 @@ function nhanvien_tamtru_tinh(id_tinh) {
 function nhanvien_tamtru_huyen(id_huyen) {
     $("#nhanvien_tamtru_huyen").click(function (event) {
         if ($(id_huyen).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_quan_huyen', {id: $("#nhanvien_tamtru_tinh").val()}, function (json) {
+            $.post('/danh_sach_quan_huyen', {id: $("#nhanvien_tamtru_tinh").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_quanhuyens' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -250,7 +250,7 @@ function nhanvien_tamtru_huyen(id_huyen) {
 function nhanvien_tamtru_xa(id_xa) {
     $("#nhanvien_tamtru_xa").click(function (event) {
         if ($(id_xa).find("option:first-child").attr('key') == 0) {
-            $.post('danh_sach_phuong_xa', {id: $("#nhanvien_tamtru_huyen").val()}, function (json) {
+            $.post('/danh_sach_phuong_xa', {id: $("#nhanvien_tamtru_huyen").val()}, function (json) {
 
                 string = "<option><%= t 'nhanviens.prompt_phuongxas' %></option>";
                 for (i = 0; i < json.length; i++) {
@@ -265,7 +265,7 @@ function nhanvien_tamtru_xa(id_xa) {
 function nhanvien_noicapcmnd(id_tinh) {
     $("#nhanvien_noicapcmnd").click(function (event) {
         if ($(id_tinh).find("option:first-child").attr('key') == 0) {
-            $.get('danh_sach_tinh_tp', function (json) {
+            $.get('/danh_sach_tinh_tp', function (json) {
                 string = "<option><%= t 'nhanviens.prompt_tinhthanhphos' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -279,7 +279,7 @@ function nhanvien_noicapcmnd(id_tinh) {
 function nhanvien_nganhang(id_nganhang) {
     $("#nhanvien_nganhang").click(function (event) {
         if ($(id_nganhang).find("option:first-child").attr('key') == 0) {
-            $.get('nganhang', function (json) {
+            $.get('/nganhang', function (json) {
                 string = "<option><%= t 'nhanviens.prompt_nganhang' %></option>";
                 for (i = 0; i < json.length; i++) {
                     string += "<option value='" + json[i].id + "'>" + json[i].ten + "</option>";
@@ -289,4 +289,3 @@ function nhanvien_nganhang(id_nganhang) {
         }
     });
 }
-
